ca8f6fb18314157e7b722d9dbd87de63117d5c50
Limon <mhlimon570@gmail.com>
2025-01-07 16:29:15 +0600

    IMPLEMENT Model

--- attendence/models.py
+++ attendence/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime, time
 
 class Employee(models.Model):
-    emp_id = models.CharField(max_length=10, unique=True)
+    emp_id = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
